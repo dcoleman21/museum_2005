c055bfdfa7bc921208c455f37807207b67165a79
Dani Coleman <danicoleman00@gmail.com>
2020-05-27 09:38:51 -0600

    Add test it has attributes

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -9,7 +9,14 @@ class MuseumTest < MiniTest::Test
   def test_it_exists
     dmns = Museum.new("Denver Museum of Nature and Science")
 
-    assert_instance_of Museum, dmns 
+    assert_instance_of Museum, dmns
+  end
+
+  def test_it_has_attributes
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_equal "Denver Museum of Nature and Science", dmns.name
+    assert_equal [], dmns.exhibits 
   end
 
 end

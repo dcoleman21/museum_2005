279e723c6d21917ab0f0f84b7837bf2f31a9aafb
Dani Coleman <danicoleman00@gmail.com>
2020-05-27 10:56:47 -0600

    Nothing changed

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -22,8 +22,8 @@ class PatronTest < MiniTest::Test
     patron_1 = Patron.new("Bob", 20)
 
     assert_equal [], patron_1.interests
-    patron_1.add_interests("Dead Sea Scrolls")
-    patron_1.add_interests("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
     assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], patron_1.interests
   end
 end

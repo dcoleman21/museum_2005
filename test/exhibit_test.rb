d3322897042f28c162bdb928d7d240d77b45343c
Dani Coleman <danicoleman00@gmail.com>
2020-05-27 09:09:36 -0600

    Add Exhibit test

---
+++ test/exhibit_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+
+class ExhibitTest < MiniTest::Test
+
+  # def test_it_exists
+  #
+  #
+  # end
+
+
+end

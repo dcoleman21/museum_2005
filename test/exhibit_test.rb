36a28cbadcaa6eba7d615ea617e583d502f181fe
Dani Coleman <danicoleman00@gmail.com>
2020-05-27 09:13:30 -0600

    Add exhibit instance

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -5,10 +5,11 @@ require './lib/patron'
 
 class ExhibitTest < MiniTest::Test
 
-  # def test_it_exists
-  #
-  #
-  # end
+  def test_it_exists
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_instance_of Exhibit, exhibit 
+  end
 
 
 end

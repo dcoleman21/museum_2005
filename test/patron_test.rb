bb47b9184f9837d7a2219cceeb0abf7a6dfe401d
Dani Coleman <danicoleman00@gmail.com>
2020-05-27 09:30:22 -0600

    Add test it has interests

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -18,4 +18,13 @@ class PatronTest < MiniTest::Test
     assert_equal 20, patron_1.spending_money
   end
 
+  def test_it_has_interests
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal [], patron_1.interests
+    patron_1.add_interests("Dead Sea Scrolls")
+    patron_1.add_interests("Gems and Minerals")
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], patron_1.interests 
+  end
+
 end

0cfe84ebf3c867cd7e655ea937c01dde0a4375f7
Dani Coleman <danicoleman00@gmail.com>
2020-05-27 09:17:05 -0600

    Add info test

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -8,7 +8,14 @@ class ExhibitTest < MiniTest::Test
   def test_it_exists
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
-    assert_instance_of Exhibit, exhibit 
+    assert_instance_of Exhibit, exhibit
+  end
+
+  def test_it_has_info
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_equal "Gems and Minerals", exhibit.name
+    assert_equal 0, exhibit.cost
   end
 
 
